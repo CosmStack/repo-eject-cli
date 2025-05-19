ba8e8bb9a73d26a7aab23937d25073cbd93a4668
Paulo Oliveira <contato@pholiveira.dev>
2025-05-19 00:10:12 -0300

    bump version

--- src/config.ts
+++ src/config.ts
@@ -21,7 +21,7 @@ const getEncryptionAlgorithm = () => {
 export const config: Config = {
   app: {
     name: "repoeject",
-    version: "0.1.2-test-4",
+    version: "0.1.2-test-5",
   },
   github: {
     apiVersion: "2022-11-28",
